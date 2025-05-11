d04faa4062d712e5b5c7bbf2651ca461bb96f2fa
Pamali Rodrigo <155502102+PamaliRodrigo@users.noreply.github.com>
2025-05-11 11:11:16 +0530

    Update SignInCard.jsx

--- src/components/SignInCard.jsx
+++ src/components/SignInCard.jsx
@@ -37,7 +37,7 @@ function SignInCard() {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('Home'); 
+    navigate('home'); 
   };
 
   const [emailError, setEmailError] = React.useState(false);
